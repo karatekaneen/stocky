8811e2108b1434a9ae8e70b12eb4836ab902f523
karatekaneen <bauhn_@hotmail.com>
2020-07-12 16:24:28 +0200

    Exported params for signal

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,5 @@
+import Stock from '../models/Stock'
+
 export type PricePointFields = 'open' | 'close' | 'high' | 'low' | 'volume' | 'owners'
 
 export type PricePoint = {
@@ -9,4 +11,31 @@ export type PricePoint = {
 	close?: number
 	volume?: number
 	owners?: number
+}
+
+export type SignalParams = {
+	/**
+	 * The price where the signal was executed
+	 */
+	price: number
+	/**
+	 * Date when the signal was executed
+	 */
+	date: Date
+	/**
+	 * If it is a buy or sell order
+	 */
+	action: 'buy' | 'sell'
+	/**
+	 * If it was entry or exit from the market
+	 */
+	type: 'enter' | 'exit'
+	/**
+	 * Pending or already executed order?
+	 */
+	status: 'pending' | 'executed'
+	/**
+	 * The stock which the order belongs to
+	 */
+	stock: Stock
 }
